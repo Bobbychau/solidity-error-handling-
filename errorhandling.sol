69c48029a5aea05d88d3dbb424442b56cb17fdb4
Bobbychau <160461958+Bobbychau@users.noreply.github.com>
2024-02-19 12:04:33 +0530

    Update errorhandling.sol

--- errorhandling.sol
+++ errorhandling.sol
@@ -7,12 +7,15 @@ contract Error {
         // - inputs
         // - conditions before execution
         // - return values from calls to other functions
+        // - Here we are using require to check if i is greater than 100. if its not it will revert the transaction
         require(_i > 100, "Input must be greater than 100");
     }
 
     function testRevert(uint _i) public pure {
         // Revert is useful when the condition to check is complex.
         // This code does the exact same thing as the example above
+        // The function revert also does the exact same thing. It is checking if i is greater than 100 
+        // if it is it will revert the statement
         if (_i <= 100) {
             revert("Input must be less than 100");
         }
